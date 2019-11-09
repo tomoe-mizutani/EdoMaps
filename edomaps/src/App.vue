052e76d09143f9e439baedbbf4f718800e0f973f
ryanjcain <ryan_cain@cate.org>
2019-11-09 14:58:24 -0500

    Initial commit of web map vue component integrating java api

--- edomaps/src/App.vue
+++ edomaps/src/App.vue
@@ -2,20 +2,36 @@
   <div id="app">
     <!-- This is a sample page with a sample embedding -->
     <h1>Map of Japan, Edo period</h1>
-    <div class="embed-container"><small><a href="//yalemaps.maps.arcgis.com/apps/Embed/index.html?webmap=99b82cf066904c2d8cd2eeac13856609&extent=-132.3413,20.4201,-46.2085,58.0972&home=true&zoom=true&scale=true&search=true&searchextent=true&details=true&legendlayers=true&active_panel=details&disable_scroll=true&theme=light" style="color:#0000FF;text-align:left" target="_blank">View larger map</a></small><br><iframe width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Something" src="//yalemaps.maps.arcgis.com/apps/Embed/index.html?webmap=99b82cf066904c2d8cd2eeac13856609&extent=-132.3413,20.4201,-46.2085,58.0972&home=true&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&details=true&legendlayers=true&active_panel=details&disable_scroll=true&theme=light"></iframe></div>
+    <web-map />
+    <!-- <div class="embed-container"><small><a href="//yalemaps.maps.arcgis.com/apps/Embed/index.html?webmap=99b82cf066904c2d8cd2eeac13856609&extent=-132.3413,20.4201,-46.2085,58.0972&home=true&zoom=true&scale=true&search=true&searchextent=true&details=true&legendlayers=true&active_panel=details&disable_scroll=true&theme=light" style="color:#0000FF;text-align:left" target="_blank">View larger map</a></small><br><iframe width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Something" src="//yalemaps.maps.arcgis.com/apps/Embed/index.html?webmap=99b82cf066904c2d8cd2eeac13856609&extent=-132.3413,20.4201,-46.2085,58.0972&home=true&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&details=true&legendlayers=true&active_panel=details&disable_scroll=true&theme=light"></iframe></div> -->
   </div>
 </template>
 
 <script>
+import WebMap from './components/WebMap.vue';
+
 export default {
   name: 'app',
   components: {
+    WebMap
   }
 }
 </script>
 
 <style>
+html,body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
 #app {
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
